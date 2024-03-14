aa82e099e0cf2103cbdaedf1c20fae90c308fd42
Nick <nick@nfreader.net>
2024-03-14 11:16:11 -0400

    Adds EDIT_UPDATES permissions

--- src/Domain/Permissions/Data/PermissionsEnum.php
+++ src/Domain/Permissions/Data/PermissionsEnum.php
@@ -7,8 +7,9 @@ enum PermissionsEnum: int
     case VIEW_INCIDENT = (1 << 0);
     case EDIT_INCIDENT = (1 << 1);
     case POST_UPDATES  = (1 << 2);
-    case ACTIVITY_LOG  = (1 << 3);
-    case UPDATE_ROLES  = (1 << 4);
+    case EDIT_UPDATES  = (1 << 3);
+    case ACTIVITY_LOG  = (1 << 4);
+    case UPDATE_ROLES  = (1 << 5);
 
     public function getName(): string
     {
@@ -16,6 +17,7 @@ enum PermissionsEnum: int
             PermissionsEnum::VIEW_INCIDENT => 'View Incident',
             PermissionsEnum::EDIT_INCIDENT => 'Edit Incident',
             PermissionsEnum::POST_UPDATES => 'Post Updates',
+            PermissionsEnum::EDIT_UPDATES => 'Edit Updates',
             PermissionsEnum::ACTIVITY_LOG => 'Use Activity Log',
             PermissionsEnum::UPDATE_ROLES => 'Update Roles',
         };
@@ -23,7 +25,6 @@ enum PermissionsEnum: int
 
     public static function fromName(string $name)
     {
-
         return constant("self::$name");
     }
 
