8ca94c46584841fc248257707cf9282a77ed547c
Nick <nick@nfreader.net>
2024-03-20 17:41:47 -0400

    Refactors entities to be fetched into a temporary composite class that we can use to build the actual entity objects

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -16,7 +16,7 @@ class Repository
 
     public function __construct(public Connection $connection)
     {
-        if($this->entityClass) {
+        if ($this->entityClass) {
             $this->getEntityMetadata($this->entityClass);
         }
     }
@@ -24,7 +24,7 @@ class Repository
     private function getEntityMetadata(string $class)
     {
         $metadata = new ReflectionClass($class);
-        foreach($metadata->getProperties() as $p) {
+        foreach ($metadata->getProperties() as $p) {
             $this->entityMetadata[$p->getName()] = $p;
         }
     }
@@ -42,46 +42,56 @@ class Repository
     public function mapRow(array $row): mixed
     {
         array_walk($row, function (&$value, $key) {
-            if(!in_array($key, array_keys($this->entityMetadata))) {
+            if (!in_array($key, array_keys($this->entityMetadata))) {
                 throw new Exception("Trying to map column to entity property that was not expected! Key: {$key} Value: {$value}");
-            } elseif(isset($this->entityMetadata[$key]) && $this->entityMetadata[$key]->getType()->isbuiltin()) {
+            } elseif (isset($this->entityMetadata[$key]) && $this->entityMetadata[$key]->getType()->isbuiltin()) {
                 return;
             } elseif (is_null($value) && $this->entityMetadata[$key]->getType()->allowsNull()) {
                 return;
             } else {
                 $class = new ReflectionClass($this->entityMetadata[$key]->getType()->getName());
-                if($class->isEnum()) {
-                    $value = call_user_func($class->getName()."::tryFrom", $value);
+                if ($class->isEnum()) {
+                    $value = call_user_func($class->getName() . "::tryFrom", $value);
                 } else {
-                    $value = new($class->getName())($value);
+                    $value = new ($class->getName())($value);
                 }
             }
         });
         return $row;
     }
 
-    public function getResults(Result $result, ?string $class = null): array
-    {
+    public function getResults(
+        Result $result,
+        ?string $class = null,
+        ?string $method = null
+    ): array {
         $results = $result->fetchAllAssociative();
-        if($class) {
+        if ($class) {
             $this->overrideMetadata($class);
         } else {
             $class = $this->entityClass;
         }
         foreach ($results as &$r) {
-            $r = new $class(...$this->mapRow($r));
+            if ($method) {
+                $r = (new $class(...$this->mapRow($r)))->$method();
+            } else {
+                $r = new $class(...$this->mapRow($r));
+            }
         }
         return $results;
     }
 
-    public function getResult(Result $result, ?string $class = null): mixed
+    public function getResult(Result $result, ?string $class = null, ?string $method = null): mixed
     {
-        if($class) {
+        if ($class) {
             $this->overrideMetadata($class);
         } else {
             $class = $this->entityClass;
         }
-        return new $class(...$this->mapRow($result->fetchAssociative()));
+        if ($method) {
+            return (new $class(...$this->mapRow($result->fetchAssociative())))->$method();
+        } else {
+            return new $class(...$this->mapRow($result->fetchAssociative()));
+        }
     }
-
 }
