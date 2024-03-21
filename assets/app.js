5dca2cde0f4b80bffaa2934dc8238843b94ef487
Nick <nick@nfreader.net>
2024-03-21 11:24:25 -0400

    Formatting

--- assets/app.js
+++ assets/app.js
@@ -39,44 +39,45 @@ async function postAsyncForm(url, data) {
     body: JSON.stringify(Object.fromEntries(data)),
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json"
-    }
+      Accept: "application/json",
+    },
   });
   return await response.json();
 }
 
 const handleFormSubmission = (form) => {
-  const indicator = form.querySelector('.indicator') ?? null
+  const indicator = form.querySelector(".indicator") ?? null;
   const asyncdata = new FormData(form);
   console.log(asyncdata);
   postAsyncForm(form.getAttribute("action"), asyncdata).then((response) => {
-    if(response.error && indicator){
-      indicator.classList.remove('visually-hidden')
-      indicator.classList.remove('text-success')
-      indicator.classList.remove('fa-check')
-      indicator.classList.add('text-danger')
-      indicator.classList.add('fa-times')
+    if (response.error && indicator) {
+      indicator.classList.remove("visually-hidden");
+      indicator.classList.remove("text-success");
+      indicator.classList.remove("fa-check");
+      indicator.classList.add("text-danger");
+      indicator.classList.add("fa-times");
     }
   });
-}
+};
 
 const asyncForms = document.querySelectorAll(".form-async");
 const asyncFormList = [...asyncForms].map((form) => {
   form.addEventListener("change", (e) => {
     if (form.dataset.autosubmit) {
-      handleFormSubmission(form)
+      handleFormSubmission(form);
     }
   });
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    handleFormSubmission(form)
+    handleFormSubmission(form);
   });
 });
 
-const form = document.querySelector('#sudoMode')
-form.addEventListener('submit', function(e){
+const form = document.querySelector("#sudoMode");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  fetch("/user/sudo",{method:"POST"})
-  setTimeout(() => {window.location.reload()}, 100);
-})
-
+  fetch("/user/sudo", { method: "POST" });
+  setTimeout(() => {
+    window.location.reload();
+  }, 100);
+});
